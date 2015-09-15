ff12028684470af16388e6d55abaf94aa8ca75ac
Xusheng Luo <freefish_6174@126.com>
2015-09-15 10:47:14 +0800

    modification

--- src/xusheng/nell/Belief.java
+++ src/xusheng/nell/Belief.java
@@ -11,8 +11,8 @@ public class Belief {
     public Belief(String line) {
         String[] spt = line.split("\t");
         entity = spt[0];
-        relation = spt[2];
-        value = spt[3];
+        relation = spt[1];
+        value = spt[2];
         if (relation.equals("generalizations")) isType = true;
     }
 
