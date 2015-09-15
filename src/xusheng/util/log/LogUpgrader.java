fa01700b91a04a5e2cc9646fa5b7771d1cf187fe
Xusheng Luo <freefish_6174@126.com>
2015-09-15 10:49:59 +0800

    modification

--- src/xusheng/util/log/LogUpgrader.java
+++ src/xusheng/util/log/LogUpgrader.java
@@ -6,7 +6,8 @@ public class LogUpgrader {
 	
 	public static void showLine(int lines, int mod) {
 		if (0 == lines % mod) {
-            if (lines > 10000) LogInfo.logs("Current: %d,0000", lines / 10000);
+            if (lines >= 100000000) LogInfo.logs("Current: %d,0000,0000", lines / 100000000);
+            else if (lines >= 10000) LogInfo.logs("Current: %d,0000", lines / 10000);
             else LogInfo.logs("Current: %d", lines);
         }
 
