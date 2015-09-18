67496902940d0d1378f1445b82ad49c83e5ef80a
angrymidiao <freefish_6174@126.com>
2015-09-19 00:21:00 +0800

    modification

--- src/xusheng/modeling2015/TransformTable.java
+++ src/xusheng/modeling2015/TransformTable.java
@@ -1,6 +1,7 @@
 package xusheng.modeling2015;
 
 import fig.basic.LogInfo;
+import sun.rmi.runtime.Log;
 import xusheng.util.struct.Pair;
 
 import java.io.BufferedReader;
@@ -39,25 +40,58 @@ public class TransformTable {
     }
 
     public static void readFile(String inFile1, String inFile2, String outFile) throws Exception {
-
+        BufferedReader br = new BufferedReader(new FileReader(inFile1));
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] spt = line.split("\t");
+            citeSet.add(spt[0]);
+            citeSet.add(spt[1]);
+            Pair<String, String> pair = new Pair<>(spt[0], spt[1]);
+            dist.put(pair, Double.parseDouble(spt[2]));
+            pair = new Pair<>(spt[1], spt[0]);
+            dist.put(pair, Double.parseDouble(spt[2]));
+        }
+        br.close();
     }
 
-    public static void calcuPath() {
+    public static void calcuPath(String outFile) throws Exception{
         for (String mid: citeSet)
             for (String st: citeSet)
-                for (String ed: citeSet)
-                    if (!st.equals(ed) && !mid.equals(st) && !mid.equals(ed)) {
-                        double dis = dist.get(new Pair<>(st, ed));
+                for (String ed: citeSet) {
+                    Pair<String, String> p1 = new Pair<>(st, mid);
+                    Pair<String, String> p2 = new Pair<>(mid, ed);
+                    if (!st.equals(ed) && !mid.equals(st) && !mid.equals(ed)
+                            && dist.containsKey(p1) && dist.containsKey(p2)) {
                         double sum = dist.get(new Pair<>(st, mid)) + dist.get(new Pair<>(mid, ed));
-                        if (dis < sum) {
+                        Pair<String, String> pair = new Pair<>(st, ed);
+                        if (dist.containsKey(pair)) {
+                            double dis = dist.get(pair);
+                            if (dis < sum) {
+                                LogInfo.logs("Update dist <%s ==> %s, %f> : <%s ==> %s ==> %s, %f>",
+                                        st, ed, dis, st, mid, ed, sum);
+                                dist.put(new Pair<>(st, ed), sum);
+                                dist.put(new Pair<>(ed, st), sum);
+                            }
+                        } else {
+                            LogInfo.logs("Add dist <%s ==> %s> : <%s ==> %s ==> %s, %f>",
+                                    st, ed, st, mid, ed, sum);
                             dist.put(new Pair<>(st, ed), sum);
                             dist.put(new Pair<>(ed, st), sum);
                         }
                     }
+                }
+        LogInfo.logs("Path complete, now write ret into file %s...", outFile);
+        BufferedWriter bw = new BufferedWriter(new FileWriter(outFile));
+        for (String st : citeSet)
+            for (String ed : citeSet)
+                bw.write(st + "\t" + ed + "\t" + dist.get(new Pair<>(st, ed)) + "\n");
+        bw.close();
+        LogInfo.logs("Complete.");
     }
 
     public static void main(String[] args) throws Exception {
-        processSingleFile(args[0], args[1]);
-        //calcuPath();
+        //processSingleFile(args[0], args[1]);
+        readFile(args[0], args[1], args[2]);
+        calcuPath(args[3]);
     }
 }
