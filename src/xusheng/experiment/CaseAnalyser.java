44b7502a4f17cadba99674d570e27467dcb782a2
angrymidiao <freefish_6174@126.com>
2015-11-24 10:54:38 +0800

    modification

--- src/xusheng/experiment/CaseAnalyser.java
+++ src/xusheng/experiment/CaseAnalyser.java
@@ -65,7 +65,7 @@ public class CaseAnalyser {
             else pos = false;
             line = br.readLine();
             spt = line.split(" ");
-            if (spt[450].equals("1") || spt[437].equals("1"))
+            if (spt[450].equals("1") || spt[437].equals("1") || spt[390].equals("1") || spt[123].equals("1") || spt[449].equals("1"))
                 if (pos) TP += 1;
                 else FP += 1;
             else if (pos) FN +=1;
