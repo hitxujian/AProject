8dd0ead3e9664035087da5bbe949005ae0ae3f0b
Xusheng Luo <freefish_6174@126.com>
2016-01-23 16:09:11 +0800

    modification

--- src/xusheng/experiment/PattyParaFuzzyMatcher.java
+++ src/xusheng/experiment/PattyParaFuzzyMatcher.java
@@ -240,11 +240,11 @@ public class PattyParaFuzzyMatcher implements Runnable {
     public static boolean hasIntersectEP(int idxA, int idxB) {
         HashSet<String> setA = instances.get(idxA);
         HashSet<String> setB = instances.get(idxB);
-        if (setA.size() < 100 || setB.size() < 100) return false;
+        if (setA.size() < 20 || setB.size() < 20) return false;
         int cnt = 0;
         for (String str: setA)
             if (setB.contains(str))  cnt ++;
-        if (cnt > 5) return true;
+        if (cnt > 2) return true;
         else return false;
     }
 
