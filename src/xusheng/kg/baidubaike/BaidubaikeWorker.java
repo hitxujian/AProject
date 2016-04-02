db02c88706feb5fec1e0d863169dbf141540d000
angrymidiao <freefish_6174@126.com>
2016-04-02 22:55:22 +0800

    modification

--- src/xusheng/kg/baidubaike/BaidubaikeWorker.java
+++ src/xusheng/kg/baidubaike/BaidubaikeWorker.java
@@ -3,6 +3,8 @@ package xusheng.kg.baidubaike;
 import fig.basic.LogInfo;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Xusheng Luo on 3/29/16.
@@ -56,7 +58,43 @@ public class BaidubaikeWorker {
         bw.close();
     }
 
+    public static void extractURLs() throws IOException {
+        Set<String> urls = new HashSet<>();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(root + "/visited.0402"));
+        int cnt = 0;
+        while (cnt < 260000) {
+            cnt += 10000;
+            String name = (cnt - 10000 + 1) + "-" + cnt;
+            for (int i = cnt - 10000 + 1; i <= cnt; i++) {
+                String fp = root + "/data_v2/" + name + "/" + i + ".html";
+                if (! new File(fp).exists()) break;
+                LogInfo.begin_track("Entering into %s...", fp);
+                BufferedReader br = new BufferedReader(new FileReader(fp));
+                String line = br.readLine();
+                if (!urls.contains(line)) {
+                    urls.add(line);
+                    bw.write(line + "\n");
+                }
+                br.close();
+            }
+        }
+        bw.close();
+        Set<String> todo = new HashSet<>();
+        BufferedReader br = new BufferedReader(new FileReader(root + "/data_v2/todo.txt"));
+        bw = new BufferedWriter(new FileWriter(root + "/unvisited.0402"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            if (!urls.contains(line) && !todo.contains(line)) {
+                todo.add(line);
+                bw.write(line + "\n");
+            }
+        }
+        br.close();
+        bw.close();
+    }
+
     public static void main(String[] args) throws IOException {
-        extractInfobox();
+        //extractInfobox();
+        extractURLs();
     }
 }
