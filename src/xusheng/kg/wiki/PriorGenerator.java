0cf4f61372ee1ef2997cc37a11454f0ca1c97600
Xusheng Luo <freefish_6174@126.com>
2016-11-13 19:20:05 +0800

    modification

--- src/xusheng/kg/wiki/PriorGenerator.java
+++ src/xusheng/kg/wiki/PriorGenerator.java
@@ -13,18 +13,22 @@ import java.util.regex.Pattern;
 
 /**
  * Created by Xusheng on 23/10/2016.
- * Calculate the prior probability of P(e|m)
- * Extract all the [[e|m]] from the raw wikipedia xml
+ * 1. Calculate the prior probability of P(e|m)
+ * by extract all the [[e|m]] from the raw wikipedia xml
+ * 2. Calculate the popularity of all entities
+ * by counting their mentioning times
  */
 public class PriorGenerator {
 
     public static String rootFp = "/home/xusheng";
     public static Map<String, HashMap<String, Integer>> appearMap = new HashMap<>();
+    public static Map<String, Integer> entTimeMap = new HashMap<>();
 
     public static void generate() throws IOException {
         File f = new File(rootFp + "/wikipedia/enwiki-20160920-pages-articles-multistream.xml");
         BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF-8"));
-        f = new File(rootFp + "/nn/data/wikipedia/prior.txt");
+        //f = new File(rootFp + "/nn/data/wikipedia/prior.txt");
+        f = new File(rootFp + "/nn/data/wikipedia/popularity.txt");
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "UTF-8"));
         String line; int cnt = 0;
         while ((line = br.readLine()) != null) {
@@ -41,19 +45,21 @@ public class PriorGenerator {
                     entity = spt[0].toLowerCase();
                     mention = spt[1];
                 }
-                add2Map(entity, mention);
+                //add2apperMap(entity, mention);
+                add2entTimeMap(entity);
                 if (cnt < 100) LogInfo.logs("[link]\t" + mention + " : " + entity);
             }
-
+            /*
             pattern = Pattern.compile("<title>(.*?)</title>");
             matcher = pattern.matcher(line);
             while (matcher.find()) {
                 String raw = matcher.group(1);
-                add2Map(raw.toLowerCase(), raw);
+                add2apperMap(raw.toLowerCase(), raw);
                 if (cnt < 100) LogInfo.logs("[title]\t" + raw + " : " + raw.toLowerCase());
             }
+            */
         }
-
+        /*
         for (Map.Entry<String, HashMap<String, Integer>> entry: appearMap.entrySet()) {
             String mention = entry.getKey();
             HashMap<String, Integer> entityMap = entry.getValue();
@@ -64,12 +70,17 @@ public class PriorGenerator {
             for (int i=0; i<sorted.size(); i++)
                 bw.write(String.format("%s\t%s\t%.4f\n", mention, sorted.get(i).getKey(),
                         (double) sorted.get(i).getValue() / sum));
+        }*/
+
+        for (Map.Entry<String, Integer> entry: entTimeMap.entrySet()) {
+            bw.write(entry.getKey() + "\t" + entry.getValue() + "\n");
         }
+
         br.close();
         bw.close();
     }
 
-    public static void add2Map(String entity, String mention) {
+    public static void add2apperMap(String entity, String mention) {
         if (!appearMap.containsKey(mention))
             appearMap.put(mention, new HashMap<>());
         if (!appearMap.get(mention).containsKey(entity))
@@ -79,6 +90,16 @@ public class PriorGenerator {
             appearMap.get(mention).put(entity, tmp);
         }
     }
+
+    public static void add2entTimeMap(String entity) {
+        if (!entTimeMap.containsKey(entity))
+            entTimeMap.put(entity, 1);
+        else {
+            int tmp = entTimeMap.get(entity) + 1;
+            entTimeMap.put(entity, tmp);
+        }
+    }
+
 
     public static void main(String[] args) throws IOException {
         generate();
